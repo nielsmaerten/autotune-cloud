cf0cf7f485b9c7df3790eca4ce782c2f35ec09e2
Niels Maerten <niels@maerten.be>
2020-01-02 23:28:53 +0100

    update gcp deploy script

--- docker/gcp-deploy.sh
+++ docker/gcp-deploy.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
 # Run this script in GCP Console to build and deploy the docker container to GCP Container Registry
-# curl -s https://raw.githubusercontent.com/nielsmaerten/autotune-docker-cloud/master/docker/gcp-deploy.sh | bash
+# curl -s https://raw.githubusercontent.com/nielsmaerten/autotune-cloud/master/docker/gcp-deploy.sh | bash
 
-rm -rf autotune-docker-cloud/
-git clone https://github.com/nielsmaerten/autotune-docker-cloud
-cd autotune-docker-cloud/docker
-export PROJECT_ID="$(gcloud config get-value project -q)"
+rm -rf autotune-cloud/
+git clone https://github.com/nielsmaerten/autotune-cloud
+cd autotune-cloud/docker
+
+# My project ID on google cloud is 'autotune-cloud', replace it here if needed
+# export PROJECT_ID="$(gcloud config get-value project -q)"
+export PROJECT_ID=autotune-cloud
+
 docker build -t gcr.io/${PROJECT_ID}/autotune .
 docker push gcr.io/${PROJECT_ID}/autotune
